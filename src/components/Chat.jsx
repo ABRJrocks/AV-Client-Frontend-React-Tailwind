32502ea6c49691dedaeb4df90f57835ac5e1296a
ABRJrocks <40339959+ABRJrocks@users.noreply.github.com>
2023-09-19 02:29:39 +0500

    Message Loading Animation Added

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -7,7 +7,7 @@ import Logout from "../assets/logoff.svg";
 import User from "../assets/user.png";
 import Assistant from "../assets/assistant.svg";
 import Filter from "../assets/filter.svg";
-import History from '../assets/history.png'
+import History from "../assets/history.png";
 import { Link } from "react-router-dom";
 
 const Chat = () => {
@@ -15,34 +15,37 @@ const Chat = () => {
   const [messages, setMessages] = useState([]);
   const endOfMessages = useRef(null);
   const [show, setShow] = useState(true);
-  
+  const [loading, setLoadin] = useState(false);
 
   const showPrevConvo = () => {
     const preConver = true;
-  }
+  };
 
   const clearMessages = () => {
-    setMessages([])
-  }
+    setMessages([]);
+  };
 
   const handleSend = () => {
     if (input.trim() === "") return;
 
     setMessages([...messages, { text: input, isUser: true }]);
-
+    setLoadin(true);
     setInput("");
 
     fetch(
-      `https://infinite-retreat-73092-419cad09f4c1.herokuapp.com/va/response/${input}`
+      //   `https://infinite-retreat-73092-419cad09f4c1.herokuapp.com/va/response/${input}`
+      // )
+      `https://jsonplaceholder.typicode.com/todos/${input}`
     )
-      // `https://jsonplaceholder.typicode.com/todos/${input}`)
       .then((response) => {
         if (!response.ok) {
           throw new Error("Failed to get a response from the server.");
         }
+        setLoadin(false);
         return response.text();
       })
       .then((data) => {
+        setLoadin(false);
         setMessages((prevMessages) => [
           ...prevMessages,
           { text: data, isUser: false },
@@ -50,6 +53,7 @@ const Chat = () => {
       })
       .catch((error) => {
         console.error("Error sending message:", error);
+        setLoadin(false);
         setMessages((prevMessages) => [
           ...prevMessages,
           { text: "Sorry, an error occurred.", isUser: false },
@@ -58,7 +62,7 @@ const Chat = () => {
   };
 
   useEffect(() => {
-    endOfMessages.current?.scrollIntoView();
+    endOfMessages.current?.scrollIntoView({ behavior: "smooth" });
   }, [messages]);
 
   const Examples = [
@@ -70,32 +74,29 @@ const Chat = () => {
     // "Are you ChatGPT or AV?",
   ];
 
-
   return (
     <div className="min-h-screen flex flex-col bg-[#F5F5F5] md:flex-row">
       <div className="w-full md:w-[30%] h-[30%] md:h-screen bg-[#eeeeee] p-4 md:p-12">
         <div className="py-2 border-b-2 text-lg md:text-xl font-semibold border-[#89AFCC] text-left md:text-left">
           Recent Activity
         </div>
-        {
-          show ? (
-            <div className="p-3 mt-4 rounded-md h-[40%] md:h-[50%] overflow-y-clip hide-scrollbar "/>
-          ) : (
-            <div className="p-3 mt-4 rounded-md h-[40%] md:h-[50%] hover:overflow-y-scroll overflow-y-clip hide-scrollbar ">
-
-          <div className="flex flex-row rounded-[10px] md:items-center mt-2">
-            <div className="leftShape basis-1/10"></div>
-            <p className="text-[18px] ml-3  basis-3/4  cursor-pointer">
-              This is my chat History
-            </p>
-            <span className="cursor-pointer ml-5 w-[18px] basis-1/10">
-              <img src={ChatHistory}></img>
-            </span>
-            <span className="cursor-pointer ml-2 w-[18px] basis-1/10">
-              <img src={Del2}></img>
-            </span>
-          </div>
-          {/* {[1, 2, 3, 4, 5, 6, 7, 9, 22, 44].map((item, index) => (
+        {show ? (
+          <div className="p-3 mt-4 rounded-md h-[40%] md:h-[50%] overflow-y-clip hide-scrollbar " />
+        ) : (
+          <div className="p-3 mt-4 rounded-md h-[40%] md:h-[50%] hover:overflow-y-scroll overflow-y-clip hide-scrollbar ">
+            <div className="flex flex-row rounded-[10px] md:items-center mt-2">
+              <div className="leftShape basis-1/10"></div>
+              <p className="text-[18px] ml-3  basis-3/4  cursor-pointer">
+                This is my chat History
+              </p>
+              <span className="cursor-pointer ml-5 w-[18px] basis-1/10">
+                <img src={ChatHistory}></img>
+              </span>
+              <span className="cursor-pointer ml-2 w-[18px] basis-1/10">
+                <img src={Del2}></img>
+              </span>
+            </div>
+            {/* {[1, 2, 3, 4, 5, 6, 7, 9, 22, 44].map((item, index) => (
             <div className="flex flex-row mt-3 border-b-[1px] border-[#E6E6E6]">
               <p className="text-[18px] ml-3 basis-4/5 cursor-pointer">
                 This is my Chat History
@@ -105,25 +106,21 @@ const Chat = () => {
               </span>
             </div>
           ))} */}
-        </div>
-          )
-        }
+          </div>
+        )}
 
         <div className="h-[30%] md:h-[10%] mt-5">
           <div className="text-[18px] font-[600] mt-[20%]">
             <div onClick={() => setShow(!show)} className="flex flex-row">
-              {
-                show ? (
-              <p className=" ml-3  basis-3/4 cursor-pointer">
-              Show Previous Conversations
-              </p>
-
-                ) : (
-              <p className=" ml-3  basis-3/4 cursor-pointer">
-              Hide Previous Conversations
-              </p>
-                )
-              }
+              {show ? (
+                <p className=" ml-3  basis-3/4 cursor-pointer hover:text-[#000000]">
+                  Show Previous Conversations
+                </p>
+              ) : (
+                <p className=" ml-3  basis-3/4 cursor-pointer hover:text-[#000000]">
+                  Hide Previous Conversations
+                </p>
+              )}
               <span className="cursor-pointer w-[20px] mt-1 basis-1/10">
                 <img src={History}></img>
               </span>
@@ -136,7 +133,7 @@ const Chat = () => {
                 <img src={Del2}></img>
               </span>
             </div>
-            <Link to="/" className="text-[#4F4F4F] hover:text-[#818181]">
+            <Link to="/" className="text-[#4F4F4F] hover:text-[#000000]">
               <div className="flex flex-row">
                 <p className="ml-3 basis-3/4 cursor-pointer logout">Logout</p>
                 <span className="cursor-pointer w-[20px] mt-1 basis-1/10 logout">
@@ -146,7 +143,10 @@ const Chat = () => {
             </Link>
           </div>
           <div>
-            <button onClick={clearMessages} className="w-[100%] h-[55px] mt-8 text-white border rounded-[10px] bg-[#4cb469] hover:bg-[#61d381] cursor-pointer">
+            <button
+              onClick={clearMessages}
+              className="w-[100%] h-[55px] mt-8 text-white border rounded-[10px] bg-[#4cb469] hover:bg-[#61d381] cursor-pointer"
+            >
               + New chat
             </button>
           </div>
@@ -154,38 +154,96 @@ const Chat = () => {
       </div>
 
       <div className="w-full md:w-[70%]">
+
+            {/* Rendering two messages funcitons to show animation as well. */}
+
         {messages.length > 0 ? (
           <div className="h-[80vh] overflow-y-scroll pt-8">
-            {messages.map((message, index) => (
-              <div
-                key={index}
-                className={`w-[75%] mx-auto p-6 flex items-center ${
-                  message.isUser === true && "flex-row-reverse "
-                }`}
-              >
-                {message.isUser === true ? (
-                  <img
-                    className="ml-5 w-[36px] shadow-md rounded-full"
-                    src={User}
-                  ></img>
-                ) : (
-                  <img
-                    className=" mr-5 w-[36px] shadow-md rounded-full"
-                    src={Assistant}
-                  ></img>
-                )}
-                <div
-                  className={`leading-loose p-6 ${
-                    message.isUser === false
-                      ? "bg-white shadow-md rounded-[10px]"
-                      : " text-white bg-[#4cb469] rounded-[10px] shadow-md"
-                  }`}
-                >
-                  {message.text}
-                  <div ref={endOfMessages} />
+            {loading ? (
+              <div>
+                <div>
+                  {messages.map((message, index) => (
+                    <div
+                      key={index}
+                      className={`w-[75%] mx-auto p-6 flex items-center ${
+                        message.isUser === true && "flex-row-reverse "
+                      }`}
+                    >
+                      {message.isUser === true ? (
+                        <img
+                          className="ml-5 w-[36px] shadow-md rounded-full"
+                          src={User}
+                        ></img>
+                      ) : (
+                        <img
+                          className=" mr-5 w-[36px] shadow-md rounded-full"
+                          src={Assistant}
+                        ></img>
+                      )}
+                      <div
+                        className={`leading-loose p-6 ${
+                          message.isUser === false
+                            ? "bg-white shadow-md rounded-[10px]"
+                            : " text-white bg-[#4cb469] rounded-[10px] shadow-md"
+                        }`}
+                      >
+                        <div>{message.text}</div>
+                        <div ref={endOfMessages} />
+                      </div>
+                    </div>
+                  ))}
+                </div>
+
+                <div class=" bg-white shadow-md rounded-[10px] p-4 max-w-sm w-full ml-[15%] h-[90px]">
+                  <div class="animate-pulse flex space-x-4">
+                    <div class="rounded-full bg-slate-200 h-10 w-10"></div>
+                    <div class="flex-1 space-y-2 py-1">
+                      <div class="h-2 bg-slate-200 rounded"></div>
+                      <div class="space-y-3">
+                        <div class="grid grid-cols-3 gap-4">
+                          <div class="h-2 bg-slate-200 rounded col-span-2"></div>
+                          <div class="h-2 bg-slate-200 rounded col-span-1"></div>
+                        </div>
+                        <div class="h-2 bg-slate-200 rounded"></div>
+                      </div>
+                    </div>
+                  </div>
                 </div>
               </div>
-            ))}
+            ) : (
+              <div>
+                {messages.map((message, index) => (
+                  <div
+                    key={index}
+                    className={`w-[75%] mx-auto p-6 flex items-center ${
+                      message.isUser === true && "flex-row-reverse "
+                    }`}
+                  >
+                    {message.isUser === true ? (
+                      <img
+                        className="ml-5 w-[36px] shadow-md rounded-full"
+                        src={User}
+                      ></img>
+                    ) : (
+                      <img
+                        className=" mr-5 w-[36px] shadow-md rounded-full"
+                        src={Assistant}
+                      ></img>
+                    )}
+                    <div
+                      className={`leading-loose p-6 ${
+                        message.isUser === false
+                          ? "bg-white shadow-md rounded-[10px]"
+                          : " text-white bg-[#4cb469] rounded-[10px] shadow-md"
+                      }`}
+                    >
+                      <div>{message.text}</div>
+                      <div ref={endOfMessages} />
+                    </div>
+                  </div>
+                ))}
+              </div>
+            )}
           </div>
         ) : (
           <div className="h-[80%] border flex flex-col justify-center items-center ">
